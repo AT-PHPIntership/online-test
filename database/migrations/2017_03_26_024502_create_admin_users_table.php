03e16d3421cf522f034b2f284cfbe420e1e86bbd
at-dinhnhan <dinhnhandtu@gmail.com>
2017-04-10 16:30:50 +0700

    change table name

--- database/migrations/2017_03_26_024502_create_admin_users_table.php
+++ database/migrations/2017_03_26_024502_create_admin_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersAdminTable extends Migration
+class CreateAdminUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_admin', function(Blueprint $table){
+        Schema::create('admin_users', function(Blueprint $table){
             $table->increments("id");
             $table->string("email")->unique();
             $table->string("password");
@@ -32,6 +32,6 @@ class CreateUsersAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_admin');
+        Schema::dropIfExists('admin_users');
     }
 }
