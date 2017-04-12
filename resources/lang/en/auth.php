3783deca7cf3c0b07f1315d4a29b3750a8feceba
at-dinhnhan <dinhnhandtu@gmail.com>
2017-04-12 08:45:48 +0700

    lables

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Please, checkout username or password and login again!.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
