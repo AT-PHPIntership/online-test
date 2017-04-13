e58afbb35e5e35ffe1aa5249199f1880a7ce7d29
at-dinhnhan <dinhnhandtu@gmail.com>
2017-04-13 17:08:19 +0700

    fix model user

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\UserExam;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','sex', 'birthday'
+    ];
+
+    public $timestamps = true;
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    /**
+     * User has many users_exam
+     *
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function userExams()
+    {
+        return $this->hasMany(UserExams::class);
+    }
+}
