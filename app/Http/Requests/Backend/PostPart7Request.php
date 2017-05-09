d1eff7e890cdf7195a6357a8122ddb018320836d
at-tinhdang <tinhdt1992@gmail.com>
2017-05-09 17:02:09 +0700

    TEMP

---
+++ app/Http/Requests/Backend/PostPart7Request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Backend;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostPart7Request extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'questions.*.content.question.*'=>'required',
+            'questions.*.image'=>'required|image',
+            'questions.*.content.answer.*.*'=>'required',
+            'questions.*.content.correct.*'=>'required',
+        ];
+    }
+
+}
